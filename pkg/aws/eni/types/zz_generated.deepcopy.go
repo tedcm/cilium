c8e6612bc86dbff2891c0198e375983567d9b281
Hemanth Malla <hemanth.malla@datadoghq.com>
2022-06-24 08:04:47 -0400

    Add support for disabling ENI PD at node level
    Currently prefix delegation (PD) can only be enabled cluster wide. On
    nodes with low pod density, PD can be wasteful in terms of number of
    unused IP addresses allocated to the node. This commit adds a new field
    to the ENI spec on ciliumnode CRD to opt out of PD at a node level.
    Other IPAM settings like pre and min allocate are already defined on the
    CRD.
    
    Signed-off-by: Hemanth Malla <hemanth.malla@datadoghq.com>

--- pkg/aws/eni/types/zz_generated.deepcopy.go
+++ pkg/aws/eni/types/zz_generated.deepcopy.go
@@ -120,6 +120,11 @@ func (in *ENISpec) DeepCopyInto(out *ENISpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.DisablePrefixDelegation != nil {
+		in, out := &in.DisablePrefixDelegation, &out.DisablePrefixDelegation
+		*out = new(bool)
+		**out = **in
+	}
 	return
 }
 
